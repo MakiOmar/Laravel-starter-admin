bb7fe2e7098037937539b8c2be68470d99f2fb4d
makiomar <mo7amed.maki@gmail.com>
2025-01-17 15:53:52 +0200

    Move scripts/styles

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -46,61 +46,12 @@
 
 {{-- Add common Javascript/Jquery code --}}
 @push('js')
-    <script src="{{ asset('assets/js/flatpickr.js') }}"></script>
-    <script src="{{ asset('assets/js/popperjs.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/fontawesome-all.js') }}"></script>
-    <script src="{{ asset('build/assets/app-CrG75o6_.js') }}"></script>
-    <script>
-        jQuery(document).ready(function ($) {
-            const columnThreshold = 7; // Number of columns to keep visible
-            // Target all tables with more than the threshold columns
-            $(document).ready(function () {
-                const columnStart = 5; // Starting column index to hide
-                const columnEnd = 9; // Ending column index to hide
-
-                $('table').each(function () {
-                    const table = $(this);
-                    const rows = table.find('tr');
-
-                    // Create and inject the toggle button
-                    const toggleButton = $('<div>')
-                        .html('<i class="fa fa-chevron-right"></i>')
-                        .attr('href', '#')
-                        .addClass('btn btn-primary') // Optional styling
-                        .css('margin-bottom', '10px') // Add some spacing
-                        .click(function (e) {
-                            e.preventDefault();
-                            // Toggle specific columns
-                            rows.each(function () {
-                                $(this)
-                                    .find(`th:nth-child(n+${columnStart}):nth-child(-n+${columnEnd}), td:nth-child(n+${columnStart}):nth-child(-n+${columnEnd})`)
-                                    .toggleClass('d-none');
-                            });
-                        });
-
-                    // Insert the button before the table
-                    table.before(toggleButton);
-
-                    // Initially hide columns 5 to 9
-                    rows.each(function () {
-                        $(this)
-                            .find(`th:nth-child(n+${columnStart}):nth-child(-n+${columnEnd}), td:nth-child(n+${columnStart}):nth-child(-n+${columnEnd})`)
-                            .addClass('d-none');
-                    });
-                });
-            });
-        });
-    </script>
+   
 @endpush
 
 {{-- Add common CSS customizations --}}
 
 @push('css')
-<script src="{{ asset('assets/js/sweetalert2.js') }}"></script>
-<link rel="stylesheet" href="{{ asset('assets/css/flatpickr.css') }}">
-<link rel="stylesheet" href="{{ asset('build/assets/app-DqME6eCz.css') }}">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.min.css" integrity="sha256-Qsx5lrStHZyR9REqhUF8iQt73X06c8LGIUPzpOhwRrI=" crossorigin="anonymous"><!--end::Third Party Plugin(Bootstrap Icons)--><!--begin::Required Plugin(AdminLTE)-->
 <style type="text/css">
 
     {{-- You can add AdminLTE customizations here --}}
