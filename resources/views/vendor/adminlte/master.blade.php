382a693ca677544d14870c433fda462031da9c5c
makiomar <mo7amed.maki@gmail.com>
2025-01-17 15:55:13 +0200

    Place scripts/styles and htmx features

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -8,7 +8,48 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <style id="maglev-loading-indicator-css">
+        #maglev-loading-indicator {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            opacity: 0;
+            visibility: hidden;
+            z-index: -1; /* Prevent blocking interactions */
+            transition: opacity 0.3s ease, visibility 0s 0.3s; /* Delays visibility after fade-out */
+        }
 
+        #maglev-loading-indicator.htmx-request {
+            opacity: 1;
+            visibility: visible;
+            z-index: 9999; /* Makes the overlay active during requests */
+            transition: opacity 0.3s ease; /* Instant visibility change */
+        }
+
+        .spinner {
+            width: 50px;
+            height: 50px;
+            border: 6px solid rgba(255, 255, 255, 0.3);
+            border-top-color: #ffffff;
+            border-radius: 50%;
+            animation: spin 1s linear infinite;
+        }
+
+        @keyframes spin {
+            from {
+                transform: rotate(0deg);
+            }
+            to {
+                transform: rotate(360deg);
+            }
+        }
+    </style>
     {{-- Custom Meta Tags --}}
     @yield('meta_tags')
 
@@ -43,7 +84,9 @@
                 <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
                 <link rel="stylesheet" href="{{ asset('vendor/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
                 <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
-
+                <link rel="stylesheet" href="{{ asset('assets/css/flatpickr.css') }}">
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.min.css" integrity="sha256-Qsx5lrStHZyR9REqhUF8iQt73X06c8LGIUPzpOhwRrI=" crossorigin="anonymous"><!--end::Third Party Plugin(Bootstrap Icons)--><!--begin::Required Plugin(AdminLTE)-->
                 @if(config('adminlte.google_fonts.allowed', true))
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
                 @endif
@@ -109,10 +152,16 @@
             @break
 
             @default
+                @vite(['resources/js/app.js', 'resources/css/app.css'])
                 <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-                <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
                 <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
                 <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
+                <script src="{{ asset('assets/js/sweetalert2.js') }}"></script>
+                <script src="{{ asset('assets/js/flatpickr.js') }}"></script>
+                <script src="{{ asset('assets/js/htmx.min.js') }}"></script>
         @endswitch
     @endif
 
@@ -131,6 +180,31 @@
     {{-- Custom Scripts --}}
     @yield('adminlte_js')
 
+    <!-- Loading Indicator -->
+    <div id="maglev-loading-indicator" hx-indicator>
+        <div class="spinner"></div>
+    </div>
+    <script type="text/javascript">
+        document.addEventListener(
+            "htmx:confirm",
+            function(e) {
+                if (!e.detail.target.hasAttribute('hx-confirm') || e.detail.target.hasAttribute('hx-no-swal') ) return
+                e.preventDefault();
+                Swal.fire({
+                    title: "Are you sure?",
+                    text: `${e.detail.question}`,
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, proceed!',
+                    cancelButtonText: 'Cancel'
+                }).then(function(result) {
+                    if (result.isConfirmed) e.detail.issueRequest(true) // use true to skip window.confirm
+                })
+            }
+        );
+    </script>
 </body>
 
 </html>
